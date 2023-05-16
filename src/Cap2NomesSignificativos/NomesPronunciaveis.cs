c4bd1fcedbd13b74227c934d3f979cc13e4f6164
menchio <rodrigo.filho@g.globo>
2023-05-16 13:16:52 -0300

    cap2 - nomes significativos - nomes pronunciáveis

---
+++ src/Cap2NomesSignificativos/NomesPronunciaveis.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * A ideia principal é escolher nomes que sejam pronunciáveis.
+ * 
+ * Escrever nomes que sejam pronunciáveis ajudam na comunicação.
+ */
+
+namespace CleanCode.Cap2NomesSignificativos
+{
+	public class NomesPronunciaveis
+	{
+		void Exemplo1()
+		{
+			void CrAMD() { }
+
+            void CriarAnoMesDia() { }
+
+			// Fica claro neste exemplo que para um programador se
+			// comunicar com o outro, pronunciando "CrAMD" seria algo bizarro,
+			// e até engraçado, mas não facilitaria a comunicação e nem o
+			// entendimento de um programador novo no projeto.
+
+			// Busque sempre por nomes pronunciáveis.
+        }
+	}
+}
