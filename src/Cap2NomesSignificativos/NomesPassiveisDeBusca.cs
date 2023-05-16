5831edee5cb07ad8ec7ec1acbb37dc14d7bde1b0
menchio <rodrigo.filho@g.globo>
2023-05-16 13:26:16 -0300

    cap2 - nomes significativos - nomes passiveis de busca

---
+++ src/Cap2NomesSignificativos/NomesPassiveisDeBusca.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * A ideia principal é escolher nomes que sejam fáceis de ser encontrados.
+ * 
+ * Escrever um nome que seja fácil de ser encontrado pelo control + f (windows)
+ * ou command + f (macos) ajuda a elevar a produtividade.
+ */
+namespace CleanCode.Cap2NomesSignificativos
+{
+	public class NomesPassiveisDeBusca
+	{
+		void Exemplo1()
+		{
+			// Exemplo ruim, supondo que 34 seja um valor muito específico
+			// para o seu negócio. Se você estiver procurando por algum número
+			// que por coincidencia possua o 34, será um grande problema.
+
+			int s = 0;
+			int[] t = {1,2,3};
+
+			for (int i = 0; i < 34; i++)
+			{
+				s += (t[i] * 4) / 5;
+			}
+
+			// Podemos resolver esse problema utilizando um nome para essa
+			// constante, facilitando sua busca para um cenário futuro.
+
+			const int NumeroMagicoDaEmpresa = 34;
+
+            for (int i = 0; i < NumeroMagicoDaEmpresa; i++)
+            {
+                s += (t[i] * 4) / 5;
+            }
+
+			// Neste caso, se o numero mágico da empresa mudar, a busca
+			// por esse número será muito mais simples.
+        }
+
+	}
+}
